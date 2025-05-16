214057bfb3406a5bf263d91d052e0c59286a8fa6
aoki-kazuaki <lec.vh@icloud.com>
2025-05-16 17:42:30 +0900

    最終コミット(以後更新しない)

--- src/components/pages/Todo/index.vue
+++ src/components/pages/Todo/index.vue
@@ -3,19 +3,19 @@ import { TodoLayout } from '@/components/template/TodoLayout';
 import type { TodoData } from '@/types/Todo';
 import { computed, ref, type ComputedRef } from 'vue';
 
-const todoData = ref<TodoData[]>([]);
+const todoDataRef = ref<TodoData[]>([]);
 /**todoとして完了ステータスになっており、論理削除済みでないもの */
 const todoCompleted: ComputedRef<TodoData[]> = computed(() =>
-  todoData.value.filter((todo) => todo.completed && !todo.isDeleted),
+  todoDataRef.value.filter((todo) => todo.completed && !todo.isDeleted),
 );
 
 /**todoとして未完了ステータスになっており、論理削除済みでないもの */
 const todoNotCompleted: ComputedRef<TodoData[]> = computed(() =>
-  todoData.value.filter((todo) => !todo.completed && !todo.isDeleted),
+  todoDataRef.value.filter((todo) => !todo.completed && !todo.isDeleted),
 );
 
 /**論理削除実施済みのもの */
-const todoIsDeleted: ComputedRef<TodoData[]> = computed(() => todoData.value.filter((todo) => todo.isDeleted));
+const todoIsDeleted: ComputedRef<TodoData[]> = computed(() => todoDataRef.value.filter((todo) => todo.isDeleted));
 
 /**論理削除の項目が存在している場合true */
 const todoIsDeletedEmpty = computed(() => {
@@ -24,28 +24,28 @@ const todoIsDeletedEmpty = computed(() => {
 
 /**データの登録を実施する */
 const addTodoData = (data: TodoData): void => {
-  todoData.value.push(data);
+  todoDataRef.value.push(data);
 };
 
 /**データの完了状態を切り替える */
 const itemToCompleteToggle = (id: string): void => {
-  const targetIndex = todoData.value.findIndex((todo) => todo.id === id);
+  const targetIndex = todoDataRef.value.findIndex((todo) => todo.id === id);
   if (targetIndex === -1) return alert('対象のデータが存在しませんでした');
-  todoData.value[targetIndex].completed = !todoData.value[targetIndex].completed;
+  todoDataRef.value[targetIndex].completed = !todoDataRef.value[targetIndex].completed;
 };
 
 /**データの論理削除状態の切り替え */
 const itemToIsDeleteToggle = (id: string): void => {
-  const targetIndex = todoData.value.findIndex((todo) => todo.id === id);
+  const targetIndex = todoDataRef.value.findIndex((todo) => todo.id === id);
   if (targetIndex === -1) return alert('対象のデータが存在しませんでした');
-  todoData.value[targetIndex].isDeleted = !todoData.value[targetIndex].isDeleted;
+  todoDataRef.value[targetIndex].isDeleted = !todoDataRef.value[targetIndex].isDeleted;
 };
 
 /**論理削除済みデータデータ一覧からデータを完全に削除する */
 const itemToDelete = (id: string): void => {
-  const targetIndex = todoData.value.findIndex((todo) => todo.id === id);
+  const targetIndex = todoDataRef.value.findIndex((todo) => todo.id === id);
   if (targetIndex === -1) return alert('対象のデータが存在しませんでした');
-  todoData.value.splice(targetIndex, 1);
+  todoDataRef.value.splice(targetIndex, 1);
 };
 </script>
 <template>
